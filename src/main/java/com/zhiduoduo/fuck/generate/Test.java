582e7c1becd2b161ea1d617b7e35a54e5e71020b
pwn2ownYearn <2311423937@qq.com>
2019-09-08 22:17:37 +0800

    Initial commit

---
+++ src/main/java/com/zhiduoduo/fuck/generate/Test.java
@@ -0,0 +1,13 @@
+package com.zhiduoduo.fuck.generate;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        int max=3;
+        for (int i=1;i<(max+1);i++){
+            System.out.println("i: "+i);
+        }
+
+    }
+}
